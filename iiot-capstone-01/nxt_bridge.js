00436384ff2e27bd34a1dc64e3d8dce4211462b9
Julbe26 <16523178@std.stei.itb.ac.id>
2026-04-19 13:46:46 +0700

    broker

--- iiot-capstone-01/nxt_bridge.js
+++ iiot-capstone-01/nxt_bridge.js
@@ -3,9 +3,9 @@ const { SerialPort } = require('serialport');
 
 // 1. KONFIGURASI 3 NXT (Update COM Port Julian)
 const NXT_DEVICES = [
-    { name: 'NXT-1', comPort: 'COM13' }, // DO, RE, MI
-    { name: 'NXT-2', comPort: 'COM17' }, // FA, SOL, LA
-    { name: 'NXT-3', comPort: 'COM12' }, // SI, DO_TINGGI
+    { name: 'NXT-1', comPort: 'COM16' }, // DO, RE, MI
+    { name: 'NXT-2', comPort: 'COM13' }, // FA, SOL, LA
+    { name: 'NXT-3', comPort: 'COM11' }, // SI, DO_TINGGI
 ];
 
 // Pemetaan nada ke Robot dan Port Motor (0=A, 1=B, 2=C)
@@ -36,7 +36,7 @@ const serialPorts = NXT_DEVICES.map(dev => {
 });
 
 // 4. KONEKSI MQTT (IP Lokal Julian)
-const client = mqtt.connect('mqtt://10.6.100.80:1883');
+const client = mqtt.connect('mqtt://10.213.106.37:1883');
 client.on('connect', () => {
     console.log('✅ Bridge Aktif - Menunggu Nada dari Python...');
     client.subscribe('robot/nada');
